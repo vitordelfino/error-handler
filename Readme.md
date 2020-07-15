409bdd04de0d2a93d76524c35cd3c1eca102293d
Vitor Delfino <vitor.delfino952@gmail.com>
2020-07-15 20:08:11 -0300

    chore: update readme

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 ![CI](https://github.com/vitordelfino/express-handler-errors/workflows/CI/badge.svg?branch=master)
 [![Coverage Status](https://coveralls.io/repos/github/vitordelfino/express-handler-errors/badge.svg?branch=master)](https://coveralls.io/github/vitordelfino/express-handler-errors?branch=master)
 ![](https://img.shields.io/github/last-commit/vitordelfino/express-handler-errors/master)
--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,16 @@
 module.exports = {
-  "preset": "ts-jest",
-  "testEnvironment": "node",
-  "coverageDirectory": "coverage",
-  "globals": {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  globals: {
     "ts-jest": {
-      "tsConfig": "tsconfig.json"
+      tsConfig: "tsconfig.json"
     }
   },
-  "modulePathIgnorePatterns": [
-    "dist",
-    "node_modules",
-    "coverage"
-  ],
-  "testMatch": [
-    "**/?(*.)+(spec|test).(js|ts|tsx)"
-  ],
-  "bail": 1,
-  "clearMocks": true,
-  "coverageReporters": [
-    "lcov",
-    "html",
-    "text"
-  ],
-  "coveragePathIgnorePatterns": [
-    "/node_modules/", "test"
-  ],
-}
+  modulePathIgnorePatterns: ["dist", "node_modules", "coverage"],
+  testMatch: ["**/?(*.)+(spec|test).(js|ts|tsx)"],
+  bail: 1,
+  clearMocks: true,
+  coverageReporters: ["lcov", "html", "text"],
+  coveragePathIgnorePatterns: ["/node_modules/", "test"]
+};
